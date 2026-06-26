1a7908795dfb458365269208fdf42b6c11625677
Feziek <josephclarksabado1@gmail.com>
2026-06-26 14:15:03 +0800

    feat: implement recursive contains function to check for target in nested objects

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,10 @@
-const contains = function() {
+const contains = function (obj, target) {
+  const arr = Object.values(obj);
+  if (arr.includes(target)) return true;
   
+  const objects = arr.filter((obj) => typeof obj === "object" && obj !== null);
+  return objects.some((nestedObj) => contains(nestedObj, target));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
