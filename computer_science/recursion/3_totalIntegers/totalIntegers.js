f432738a177f6a198cbf3eedc45f6aa4184d3cc8
Feziek <josephclarksabado1@gmail.com>
2026-06-28 23:27:09 +0800

    feat: add isObj function to check for object types

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,10 @@
-const totalIntegers = function() {
-  
+const isObj = (obj) => {
+  if (typeof obj === "object" && !Array.isArray(obj) && obj !== null)
+    return true;
+  else return false;
 };
-  
+
+const totalIntegers = function (arr) {};
+
 // Do not edit below this line
 module.exports = totalIntegers;
