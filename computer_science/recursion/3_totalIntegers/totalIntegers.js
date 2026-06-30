03dd09c21448db1fa169a334b85249cc0889df43
Feziek <josephclarksabado1@gmail.com>
2026-06-30 14:26:21 +0800

    feat: implement totalIntegers function to count integers in arrays and objects

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -4,7 +4,27 @@ const isObj = (obj) => {
   else return false;
 };
 
-const totalIntegers = function (arr) {};
+const totalIntegers = function (arr) {
+  let count = 0;
+  let arrFromObj = arr;
+
+  if (!Array.isArray(arr) && !isObj(arr)) return undefined;
+  if (isObj(arr)) arrFromObj = Object.values(arr);
+
+  if (arrFromObj.length === 0) return 0;
+
+  for (const item in arrFromObj) {
+    if (Number.isInteger(arrFromObj[item])) {
+      count += 1;
+    }
+
+    if (Array.isArray(arrFromObj[item]) || isObj(arrFromObj[item])) {
+      count += totalIntegers(arrFromObj[item]);
+    }
+  }
+
+  return count;
+};
 
 // Do not edit below this line
 module.exports = totalIntegers;
